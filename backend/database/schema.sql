5e196a23ca2a0b356088e6ba85b98cc65eeed0e2
Jiaqi <blacker0lily311@gmail.com>
2021-09-08 22:32:04 +1000

    fixed CHECK errors; changes all attribute names to lower case with underscore; updated style for keys; now schema works in postgresql db

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,130 +1,111 @@
-create type RoleType as enum ('Admin', 'Member');
-create type ColourType as enum ('Red', 'Orange', 'Yellow', 'Blue', 'Green', 'Grey', 'Brown', 'Purple', 'Pink');
-create type CheckoutType as enum ('Borrow', 'Use');
-create type CheckoutStatus as enum ('Waiting', 'CheckedOut', 'Returned');
-create type ApprovalStatus as enum ('PendingApproval', 'Approved', 'NotApproved');
+create type role_type as enum ('Admin', 'Member');
+create type colour_type as enum ('Red', 'Orange', 'Yellow', 'Blue', 'Green', 'Grey', 'Brown', 'Purple', 'Pink');
+create type checkout_type as enum ('Borrow', 'Use');
+create type checkout_status as enum ('Waiting', 'CheckedOut', 'Returned');
+create type approval_status as enum ('PendingApproval', 'Approved', 'NotApproved');
 
-
+-- Need to cast zid as text so regex match can work
 CREATE TABLE Person (
-    zID         integer check (zID ~ '^[1-9][0-9]{6}$'),
-    password    varchar(12) NOT NULL,
-    FirstName   varchar(30) NOT NULL,
-    LastName    varchar(30),
-    Email       varchar(50) CHECK (Email ~ '^[A-Za-z0-9]+[\._]?[A-Za-z0-9]+[@]\w+[.]\w{2,3}$') NOT NULL,
-    Phone       varchar(10) CHECK (Phone ~ '[0-9]{10}'),
-    Picture     text,
-    Role        RoleType NOT NULL,
-    PRIMARY KEY (zID)
+    zid             integer PRIMARY KEY,
+    password        varchar(12) NOT NULL,
+    first_name      varchar(30) NOT NULL,
+    last_name       varchar(30),
+    email           varchar(50) NOT NULL,
+    phone_no        varchar(10),
+    picture         text,
+    role            role_type NOT NULL,
+    CONSTRAINT CK_zid CHECK (CAST(zid as text) ~* '^[1-9][0-9]{6}$'),
+    CONSTRAINT CK_email CHECK (email ~* '^[A-Za-z0-9]+[\._]?[A-Za-z0-9]+[@]\w+[.]\w{2,3}$'),
+    CONSTRAINT CK_phone CHECK (phone_no ~* '[0-9]{10}')
 );
 
 CREATE TABLE Location (
-    id          serial,
-    Name        varchar(20) NOT NULL,
-    Description text,
-    Picture     text,
-    PRIMARY KEY (id)
+    id              serial PRIMARY KEY,
+    name            varchar(20) NOT NULL,
+    description     text,
+    picture         text
 );
 
 CREATE TABLE Item (
-    SKU          serial,
-    name         varchar(50) NOT NULL,
-    Image        text,
-    Description  text,
-    PRIMARY KEY (SKU)
+    sku             serial PRIMARY KEY,
+    name            varchar(50) NOT NULL,
+    image           text,
+    description     text
 );
 
 CREATE TABLE ItemAt (
-    SKU          integer,
-    LocationID   integer,
-    Qty          integer CHECK (Qty >= 0),
-    PRIMARY KEY (SKU, LocationID),
-    FOREIGN KEY SKU REFERENCES Item(SKU),
-    FOREIGN KEY LocationID REFERENCES Location(id),
+    sku             integer REFERENCES Item(sku),
+    location_id     integer REFERENCES Location(id),
+    qty             integer CHECK (qty >= 0),
+    PRIMARY KEY (sku, location_id)
 );
 
 CREATE TABLE Tag (
-    id          serial,
-    Name        varchar(20) NOT NULL,
-    Description text,
-    Colour      ColourType,
-    PRIMARY KEY (id)
+    id              serial PRIMARY KEY,
+    name            varchar(20) NOT NULL,
+    description     text,
+    colour          colour_type
 );
 
 CREATE TABLE ItemTags (
-    SKU          integer,
-    TagID        integer,
-    PRIMARY KEY (SKU, Tag),
-    FOREIGN KEY SKU REFERENCES Item(SKU),
-    FOREIGN KEY Tag REFERENCES Tag(id),
+    sku             integer REFERENCES Item(sku),
+    tag_id          integer REFERENCES Tag(id),
+    PRIMARY KEY (sku, tag_id)
 );
 
 CREATE TABLE Approval (
-    id           serial,
-    Status       ApprovalStatus NOT NULL,
-    ApprovedOn   timestamp,
-    ApprovedBy   integer,
-    Notes        text,
-    PRIMARY KEY (id),
-    FOREIGN KEY (ApprovedBy) REFERENCES Person(zID) 
+    id              serial PRIMARY KEY,
+    Status          approval_status NOT NULL,
+    approved_on     timestamp,
+    approved_by     integer REFERENCES Person(zid) NOT NULL,
+    Notes           text
 );
 
 CREATE TABLE Checkout (
-    id           serial,
-    Type         CheckoutType NOT NULL,
-    RequstedBy   integer NOT NULL,
-    Reason       text,
-    Status       CheckoutStatus NOT NULL,
-    LodgedOn     timestamp NOT NULL,
-    CheckedoutOn timestamp NOT NULL,
-    ReturnedOn   timestamp,      
-    PRIMARY KEY (id),
-    FOREIGN KEY (RequestedBy) REFERENCES Person(zID)
+    id              serial PRIMARY KEY, 
+    type            checkout_type NOT NULL,
+    requested_by    integer REFERENCES Person(zid) NOT NULL,
+    reason          text,
+    status          checkout_status NOT NULL,
+    lodged_on       timestamp NOT NULL,
+    checkedout_on   timestamp NOT NULL,
+    returned_on     timestamp     
 );
 
 CREATE TABLE CheckoutSummary (
-    CheckoutID   integer,
-    SKU          integer,
-    Qty          integer CHECK (Qty >= 1),
-    PRIMARY KEY (CheckoutID, SKU),
-    FOREIGN KEY (CheckoutID) REFERENCES Checkout(id),
-    FOREIGN KEY (SKU) REFERENCES Item(SKU)
+    checkout_id     integer REFERENCES Checkout(id),
+    sku             integer REFERENCES Item(sku),
+    qty             integer CHECK (qty >= 1),
+    PRIMARY KEY (checkout_id, sku)
 );
 
 CREATE TABLE CheckoutApprovals (
-    CheckoutID   integer,
-    ApprovalID   integer,
-    PRIMARY KEY (CheckoutID, ApprovalID),
-    FOREIGN KEY (CheckoutID) REFERENCES Checkout(id),
-    FOREIGN KEY (ApprovalID) REFERENCES Approval(id)
+    checkout_id     integer REFERENCES Checkout(id),
+    ApprovalId      integer REFERENCES Approval(id),
+    PRIMARY KEY (checkout_id, ApprovalId)
 );
 
 CREATE TABLE Orders (
-    id           serial,
-    LodgedBy     integer NOT NULL,
-    LodgedOn     timestamp NOT NULL,
-    Description  text,
-    FinalApprovalOn datetime,
-    PurchasedOn  datetime,
-    PRIMARY KEY (id),
-    FOREIGN KEY (LodgedBy) REFERENCES Person(zID)
+    id              serial PRIMARY KEY,
+    lodged_by       integer REFERENCES Person(zid) NOT NULL,
+    lodged_on       timestamp NOT NULL,
+    description     text,
+    approvaled_on   timestamp,
+    purchased_on    timestamp
 );
 
 CREATE TABLE OrderSummary (
-    OrderID      integer,
-    SKU          integer,
-    Qty          integer CHECK (Qty >= 1),
-    UnitPrice    float CHECK (UnitPrice >= 0),
-    Documentation text,
-    PRIMARY KEY (OrderID, SKU),
-    FOREIGN KEY (OrderID) REFERENCES Orders(id),
-    FOREIGN KEY (SKU) REFERENCES Item(SKU)
+    order_id        integer REFERENCES Orders(id),
+    sku             integer REFERENCES Item(sku),
+    qty             integer CHECK (qty >= 1),
+    UnitPrice       float CHECK (UnitPrice >= 0),
+    documentation   text,
+    PRIMARY KEY (order_id, sku)
 );
 
 CREATE TABLE OrderApprovals (
-    OrderID      integer,
-    ApprovalID   integer,
-    PRIMARY KEY (OrderID, ApprovalID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(id),
-    FOREIGN KEY (ApprovalID) REFERENCES Approval(id)
+    order_id        integer REFERENCES Orders(id),
+    ApprovalId      integer REFERENCES Approval(id),
+    PRIMARY KEY (order_id, ApprovalId)
 );
 
