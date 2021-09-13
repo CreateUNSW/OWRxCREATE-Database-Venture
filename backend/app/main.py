59e45ca0ad6268706f68bfff7205f1be5d099b2b
Anthony Carbone <anthonydavidcarbone@gmail.com>
2021-09-13 16:58:53 +0800

    added command to main.py create all tables

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,9 @@ from .database import SessionLocal, engine
 
 from . import models
 
+# uncomment below to create all tables in the postgres database
+# models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()  # run from backend directory: `uvicorn app.main:app --reload`
 
 # Dependency
