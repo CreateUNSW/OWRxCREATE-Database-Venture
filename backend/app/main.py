94d1a6775dce00c0a3259f9482a568d36e87657f
Anthony Carbone <anthonydavidcarbone@gmail.com>
2021-09-20 00:26:27 +0800

    Squashed commit of the following:
    commit 4afa01207e2df396da1ac064a119553324abe6bf
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 20 00:22:35 2021 +0800
    
        changed unnecessary varchar's in models.py to text (based on Kyu-Sang's advice)
    
    commit 9a132b1cf45e0046980003eab7ccfa89bb791d3a
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 20 00:20:28 2021 +0800
    
        updated password -> hashed_password
    
    commit 37919eb56912e12cffc6ca540eb32bc1975e56f3
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 20 00:18:46 2021 +0800
    
        removed email regex - it's too hard and ineffective - instead opt for sending verification emails
    
    commit 8aa6f8c6c366689cae52ef538df7d9242476e110
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Sun Sep 19 23:46:29 2021 +0800
    
        changed database/ zid format to include preceding z
    
    commit 322780707eea5824e8ce1eea9e95fda51a60ebb0
    Merge: 1691f9a 0d5d78d
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Sun Sep 19 23:43:48 2021 +0800
    
        Merge branch 'database' into ORM
    
    commit 1691f9a18a92964f0d0b98923d852a5d24ce5c64
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Sun Sep 19 23:42:31 2021 +0800
    
        changed zid format to include preceding z
    
    commit 59e45ca0ad6268706f68bfff7205f1be5d099b2b
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 13 16:58:53 2021 +0800
    
        added command to main.py create all tables
    
    commit 559a42d8108ca23f77fb59341d0137325a7182d2
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 13 16:10:07 2021 +0800
    
        fixed some remaining typos in models.py
    
    commit a8e63896d9a664cde51f0780740eb3f3efd1587c
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Fri Sep 10 11:55:41 2021 +0800
    
        order_ back to orders, sorry XD
    
    commit 2eec48a382185491a48dcafcedf926c0be2c0787
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Fri Sep 10 01:30:07 2021 +0800
    
        Added first working prototype - it connects to local databases!!
    
    commit 5b83b3f3149824d156f849e123715093e9dff07d
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Fri Sep 10 01:29:20 2021 +0800
    
        Added psycopg2 to Pipfile and Pipfile.lock
    
    commit 48d5d0dc9a88f8a900881e08088428d5d499d7af
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Fri Sep 10 00:51:26 2021 +0800
    
        Updated identifiers
    
    commit f64cedbc5cd0ccb94fbf42a469e5d7021e7ab436
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Fri Sep 10 00:21:24 2021 +0800
    
        applied formatter
    
    commit b5be80b3652f0d161c157f970c67351b949490b5
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Wed Sep 8 14:19:24 2021 +0800
    
        added enums
    
    commit 54514d14aae37f4ea02479f6f928ba7e8426cd5b
    Author: Anthony Carbone <anthonydavidcarbone@gmail.com>
    Date:   Mon Sep 6 14:41:07 2021 +0800
    
        add SQLAlchemy to pipfile pipfile.lock
    
    commit bf8ff27439411c958acd871a68cd159fcd19eab6
    Author: scottheb <85664839+scottheb@users.noreply.github.com>
    Date:   Mon Sep 6 13:58:31 2021 +1000
    
        Create models.py (~95%)
    
        Approx. 95% complete.
        Need to confirm method of applying constraints.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,23 @@
-# Just placeholder code for now, it's the FastAPI Hello World
-# example from their docs
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .database import SessionLocal, engine
 
-from typing import Optional
+from . import models
 
-from fastapi import FastAPI
+# uncomment below to create all tables in the postgres database
+# models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI()  # run from backend directory: `uvicorn app.main:app --reload`
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_users(db: Session = Depends(get_db)):
+    return db.query(models.Tag).first()  # Write your query here
