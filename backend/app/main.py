9056c069a0f5cebafe594e66ec777441ca23c41b
Prayag <prayag1601@gmail.com>
2021-09-23 22:55:35 +1000

    fix imports and move get_db dependency to database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,13 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from .database import SessionLocal, engine
+from database.database import SessionLocal
 
-from . import models
+from models import models
 
 # uncomment below to create all tables in the postgres database
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()  # run from backend directory: `uvicorn app.main:app --reload`
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 @app.get("/")
 def read_users(db: Session = Depends(get_db)):
