5c7e42914ed99d7c4ab51831ca2f858988dc28a3
Prayag <prayag1601@gmail.com>
2021-09-25 19:03:32 +1000

    add authorisation dependency

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,9 +1,11 @@
 import os
 from dotenv import load_dotenv
-import time
-from jose import jwt
+from datetime import datetime, timedelta
+from fastapi.security.http import HTTPBearer
+import jwt
 
 from fastapi import Depends, HTTPException, APIRouter
+from fastapi.security import HTTPBearer, HTTPBasicCredentials
 
 router = APIRouter(
     prefix="/auth",
@@ -24,52 +26,90 @@ load_dotenv()
 expiryTimeMinutes = float(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 secretJWT = os.getenv('JWT_SECRET')
 algorithmJWT = os.getenv('JWT_ALGORITHM')
+security = HTTPBearer()
 
-def createToken(userId: str):
+def createToken(zid: str):
     payload = {
-        "userId": userId,
-        "expires": time.time() + expiryTimeMinutes * 60
+        "zid": zid,
+        "expires": str(datetime.utcnow() + timedelta(minutes=expiryTimeMinutes))
     }
 
     token = jwt.encode(payload, secretJWT, algorithm=algorithmJWT)
-    return { "accessToken": token }
+    return token
+
 
 def decodeToken(token: str):
     try:
-        decodedToken = jwt.decode(token, secretJWT, algorithms=[algorithmJWT])
-        return decodedToken if decodedToken['expires'] >= time.time() else None
-    except:
-        return None
+        decodedToken = jwt.decode(token, secretJWT, algorithms=algorithmJWT)
+        expires = datetime.strptime(decodedToken['expires'], "%Y-%m-%d %H:%M:%S.%f")
+        
+        if expires >= datetime.utcnow():
+            return decodedToken
+        else:
+            raise jwt.ExpiredSignatureError
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid Token")
+
+def authorise(token: HTTPBasicCredentials = Depends(security)):
+    decoded = decodeToken(token.credentials)
+    if decoded:
+        return token
 
 # Add new person to the database
-@router.post('/person/', response_model=pydantic.Person)
-def createPerson(person: pydantic.PersonRegister, db: Session = Depends(get_db)):
+@router.post('/register/', response_model=pydantic.Person)
+def register(person: pydantic.RegisterRequest, db: Session = Depends(get_db)):
     # Check if zid already registered
-    userExists = db.query(models.Person).filter(models.Person.zid == person.zid).first()
+    userExists = db.query(models.Person).filter(
+        models.Person.zid == person.zid
+    ).first()
+
     if userExists:
         raise HTTPException(status_code=400, detail="User already exists")
     
-    hashedPassword = encrypt(person.password)
-    newPerson = models.Person(
-        zid = person.zid,
-        password = hashedPassword,
-        first_name = person.first_name,
-        last_name = person.last_name,
-        email = person.email,
-        phone = person.phone,
-        picture = person.picture,
-        role = models.RoleType(1)
-    )
+    try:
+        newUser = models.Person(
+            zid = person.zid,
+            password = encrypt(person.password),
+            first_name = person.first_name,
+            last_name = person.last_name,
+            email = person.email,
+            phone = person.phone,
+            picture = person.picture,
+            role = models.RoleType(2) if None else person.role
+        )
 
-    db.add(newPerson)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+        # Without the password (handled by person response_model)
+        return person
+    except:
+        raise HTTPException(status_code=402, detail="ERROR")
 
-    # Without the password (handled by person response_model)
-    return person
+@router.post('/login', response_model=pydantic.LoginResponse)
+def login(person: pydantic.LoginRequest, db: Session = Depends(get_db)):
+    # Ensure zid and password correct
+    user = authenticateUser(person, db)
 
-def getPersonByZid(zid, db: Session = Depends(get_db)):
-    return db.query(models.Person).filter(models.Person.zid == zid).first()
+    if user:
+        return {
+            "success": True,
+            "token": createToken(person.zid)
+        }
 
+    raise HTTPException(status_code=401, detail="Invalid username or password")
+
+def authenticateUser(person: pydantic.PersonCredentials, db: Session = Depends(get_db)):
+    return db.query(models.Person).filter(
+                models.Person.zid == person.zid,
+                models.Person.password == encrypt(person.password)
+            ).first()
+
+# Test protected route
+@router.get('/secret')
+def secret(authorise: str = Depends(authorise)):
+    return "secret resource"
 
 # Encrypt user password and return the hexadecimal representation
 def encrypt(password) :
